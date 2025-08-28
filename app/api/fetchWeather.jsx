eecc12c20288454f0f48f593d43594961cfb75d6
ganzabruce <ganzabruce10.com>
2025-08-28 07:16:57 +0200

    second deployment

--- app/api/fetchWeather.jsx
+++ app/api/fetchWeather.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const URLs = process.env.API_URL;
-const API_KEYs = process.env.API_KEY;
+const URLs = "http://api.weatherapi.com/v1/current.json";
+const API_KEYs = "463b5d5ca14c410ebed100531252708";
 console.log(URLs,API_KEYs)
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URLs, {
